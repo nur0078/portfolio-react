7e07b05541e554469a3ba7ff9f70a3fc366dd554
Anurodh <anurodhp7@gmail.com>
2023-06-28 08:56:21 +1000

    made stacks and projects components

---
+++ src/const/project.ts
@@ -0,0 +1,19 @@
+import Project1 from '../assets/project1.png'
+import Project2 from '../assets/project2.png';
+
+export const Projects = [
+    {
+      title: "Pagli Writes",
+      description: "A web application for author.",
+      tools: ["ReactJS", "Tailwind", "TypeScript", "HTML", "Figma", "Git/Github"],
+      img: Project1,
+    },
+    {
+      title: "Digital Business",
+      description: ` A digital business card.`,
+      tools: ["ReactJS", "JavaScript", "CSS", "Figma"],
+      img: Project2,
+    },
+
+];
+
---
+++ src/const/stacks.ts
@@ -0,0 +1,32 @@
+import HTML from '../assets/icons/HTML5.svg'
+import CSS from '../assets/icons/CSS.svg'
+import JS from '../assets/icons/JS.svg'
+import ReactIcon from '../assets/icons/React.svg'
+import TailIcon from '../assets/icons/Tailwind.svg'
+
+export const Stacks = [
+    {
+        src: HTML,
+        alt: "HTML LOGO"
+    },
+
+    {
+        src: CSS,
+        alt: "CSS LOGO"
+    },
+    
+    {
+        src: JS,
+        alt: "JS LOGO"
+    },
+    {
+        src: ReactIcon,
+        alt: "ReactJS LOGO"
+    },
+    {
+        src: TailIcon,
+        alt: "Tailwind LOGO"
+    },
+
+
+];
