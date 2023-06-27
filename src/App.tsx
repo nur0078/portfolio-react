5ade58aa56efc12b754da64af7b40f70feda8433
Anurodh <anurodhp7@gmail.com>
2023-06-28 08:56:41 +1000

    added new comps for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,153 +1,17 @@
 import './App.css';
-import { GiRaven } from 'react-icons/gi';
-import { BsFillMoonStarsFill, BsLinkedin, BsGithub } from 'react-icons/bs';
-import DP from '../src/assets/og.png';
-import Blog from '../src/assets/blog.png';
-import Project1 from '../src/assets/project1.png';
-import Project2 from '../src/assets/project2.png';
-import ID from '../src/assets/id.png';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <main className="bg-white px-10 font-poppins md:px-20 lg:px-40">
-        <section className="min-h-screen">
-          {/* navbar */}
-          <nav className="py-10 mb-12 flex justify-between">
-            <a href="/">
-              <h1 className="text-5xl font-caveat">
-                <span className="text-rdrPurp">a</span>nurodh
-              </h1>
-            </a>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
-              </li>
-              <li>
-                <a
-                  className=" bg-gradient-to-tr from-rdrPurp  to-nur  text-white px-5 py-2 rounded-md ml-8"
-                  href="#"
-                >
-                  {/* hover:from-dskBlk  hover:to-dskRed /// dskBlk= gray-700 */}
-                  Resume
-                </a>
-              </li>
-            </ul>
-          </nav>
-          {/* intro */}
-          <div className="text-center p-10 ">
-            <h2 className="text-5xl py-2 text-rdrPurp font-semibold md:text-6xl">Anurodh Pandey</h2>
-            <h3 className=" font-mulish text-2xl py-2 text-nur md:text-3xl"> React Dveloper </h3>
-            <p className="font-mulish text-md py-5 leading-8 text-gray-800 text-justify md:text-lg max-w-xl mx-auto">
-              Hi, I'm a Front-end React Developer from Sydney, Australia.Labore Lorem aliquip amet
-              voluptate. Qui commodo aliquip sint cupidatat in commodo nulla ullamco occaecat
-              aliquip laboris non laboris incididunt exercitation.
-            </p>
-          </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-00">
-            <a href="https://www.github.com/nur0078/" target="_blank">
-              <BsGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/pandeyanurodh/" target="_blank">
-              <BsLinkedin />
-            </a>
-            <a href="mailto:anurodhp7@gmail.com">
-              <GiRaven />
-            </a>
-          </div>
-          <div className="relative  mt-20  ">
-            <img
-              src={DP}
-              alt="Anurodh Pandey"
-              className="rounded-full w-80 h-80 mt-20 mx-auto md:h-96 md:w-96"
-            />
-          </div>
-        </section>
-        <section>add Tech Stack | images of tools</section>
-
-        <section>
-          <div>
-            <h3 className="text-3xl py-1"> My Portfolio</h3>
-            <p className="font-mulish text-md py-2 leading-8 text-gray-800 text-justify">
-              Hi, I'm a Front-end React Developer from Sydney, Australia.Labore Lorem aliquip amet
-              voluptate. Qui commodo aliquip sint cupidatat in commodo nulla ullamco occaecat
-              aliquip laboris non laboris incididunt exercitation.
-            </p>
-          </div>
-          <div className="lg:flex gap-10">
-            <a href="https://pagli-writes.vercel.app/" target="_blank">
-              <div className=" text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-                <img
-                  src={Blog}
-                  alt="Blog site for an author"
-                  width={100}
-                  height={100}
-                  className="mx-auto"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2"> Blog for an Author</h3>
-                <p className="py-2 font-mulish">
-                  A blog site for an author where they can publish their literature.
-                </p>
-                <h4 className="py-4 text-rdrPurp  ">Design Tools I Use</h4>
-                <p className="text-nur py-1">ReactJS</p>
-                <p className="text-nur py-1">TypeScript</p>
-                <p className="text-nur py-1">Tailwind CSS</p>
-                <p className="text-nur py-1">Figma</p>
-                <p className="text-nur py-1">Git/Github</p>
-              </div>
-            </a>
-            <a href="https://www.anurodhpandey.com.np/" target="_blank">
-              <div className=" text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-                <img
-                  src={ID}
-                  alt="Digital Business Card"
-                  width={100}
-                  height={100}
-                  className="mx-auto"
-                />
-                <h3 className="text-lg font-medium pt-8 pb-2"> Digital Business Card</h3>
-                <p className="py-2 font-mulish">A digital business card.</p>
-                <h4 className="py-4 text-rdrPurp ">Design Tools I Use</h4>
-                <p className="text-nur py-1">ReactJS</p>
-                <p className="text-nur py-1">Javascript</p>
-                <p className="text-nur py-1"> CSS</p>
-                <p className="text-nur py-1">Figma</p>
-              </div>
-            </a>
-          </div>
-        </section>
-        <section>
-          <div>
-            <h3 className="text-3xl py-1"> My Portfolio</h3>
-            <p className="font-mulish text-md py-2 leading-8 text-gray-800 text-justify">
-              My portfolios are below:
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:h-90 lg:flex-row lg:flex-wrap">
-            <div className="  basis-1/3 flex-1 h-90">
-              <img
-                src={Project1}
-                className="rounded-lg object-cover shadow-md"
-                width={'100%'}
-                height={'100%'}
-              />
-            </div>
-            <div className="  basis-1/3 flex-1 ">
-              <img
-                src={Project2}
-                className="rounded-lg object-cover shadow-md"
-                width={'100%'}
-                height={'100%'}
-              />
-            </div>
-          </div>
-        </section>
-      </main>
-      <footer className="  text-center text-lg p-5 mt-5 bg-gradient-to-bl from-rdrPurp  to-nur  text-white ">
-        Copyright &copy; 2023. All rights are reserved
-      </footer>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="resume" element={<Resume />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,144 @@
+import '../App.css';
+
+import { GiRaven } from 'react-icons/gi';
+import { BsFillMoonStarsFill, BsLinkedin, BsGithub } from 'react-icons/bs';
+import DP from '../assets/og.png';
+import { Projects } from '../const/project';
+import { useState } from 'react';
+import { Stacks } from '../const/stacks';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <main className="bg-white px-10 font-poppins md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          {/* navbar */}
+          <nav className="py-10 mb-12 flex justify-between">
+            <a href="/">
+              <h1 className="text-5xl font-caveat">
+                <span className="text-rdrPurp">a</span>nurodh
+              </h1>
+            </a>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <Link to="/resume">
+                  <button className=" bg-gradient-to-tr from-rdrPurp  to-nur  text-white px-5 py-2 rounded-md ml-8">
+                    Resume
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* intro */}
+          <div className="text-center p-10 ">
+            <h2 className="text-5xl py-2 text-rdrPurp font-semibold md:text-6xl">Anurodh Pandey</h2>
+            <h3 className=" font-mulish text-2xl py-2 text-nur md:text-3xl"> React Dveloper </h3>
+            <p className="font-mulish text-md py-5 leading-8 text-gray-800 text-center md:text-lg max-w-xl mx-auto">
+              Hi, I'm a Front-end Developer from Sydney, Australia.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-00">
+            <a href="https://www.github.com/nur0078/" target="_blank">
+              <BsGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/pandeyanurodh/" target="_blank">
+              <BsLinkedin />
+            </a>
+            <a href="mailto:anurodhp7@gmail.com">
+              <GiRaven />
+            </a>
+          </div>
+          <div className="relative  mt-20  ">
+            <img
+              src={DP}
+              alt="Anurodh Pandey"
+              className="rounded-full w-96 h-96 mt-20 mx-auto md:h-96 md:w-96"
+            />
+          </div>
+        </section>
+        <section className="pt-4 font-poppins text-xl">
+          <div className="flex gap-6 py-4 justify-center h-16">
+            <h1 className="items-center">Tech Stack |</h1>
+            {Stacks.map((stack, index) => (
+              <StackComponent key={index} src={stack.src} alt={stack.alt} />
+            ))}
+          </div>
+        </section>
+
+        <section className="pt-12">
+          <div className="text-center">
+            <h3 className="text-3xl py-1"> My Portfolio</h3>
+            <p className="font-mulish text-md py-2 leading-8 text-gray-800 ">
+              Hi, These are the projects I have done so far.
+            </p>
+          </div>
+          <div className="flex gap-14 flex-wrap py-4 justify-center ">
+            {Projects.map((project, index) => {
+              return <Card project={project} key={index} />;
+            })}
+          </div>
+        </section>
+      </main>
+      <footer className="  text-center text-lg p-5 mt-10 bg-gradient-to-bl   text-black ">
+        Copyright &copy; 2023. All rights are reserved
+      </footer>
+    </div>
+  );
+}
+interface Project {
+  title: string;
+  description: string;
+  tools: string[];
+  img: string; // Change this if img is not a string
+}
+interface CardProps {
+  project: Project;
+}
+
+const Card = ({ project }: CardProps) => {
+  const [isCardClicked, setIsCardClicked] = useState(false);
+
+  return (
+    <div
+      onClick={() => setIsCardClicked(!isCardClicked)}
+      className={`transform ease-in-out duration-500 hover:scale-105 rounded-lg px-5 py-4 leading-normal shadow-md shadow-slate-300 w-80 h-104 dark:text-black text-center font-poppins dark:shadow-md dark:shadow-white dark:bg-white dark:opacity-80`}
+    >
+      {isCardClicked ? (
+        <div className=" flex flex-col gap-6 shadow-md flex-1">
+          <h1 className=" text-lg ">{project.title}</h1>
+          <h2>{project.description}</h2>
+          <div className="flex gap-4 flex-wrap  ">
+            {project.tools.map((tool: string, index: number) => {
+              return (
+                <div
+                  key={index}
+                  className=" text-center rounded shadow shadow-slate-400 px-5 py-2 bg-slate-600 text-white dark:bg-slate-50 dark:text-black"
+                >
+                  {tool}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col shadow-lg">
+          <h1 className="  p-0 text-center font-poppins text-lg text-black ">{project.title}</h1>
+          <img src={project.img} alt={project.img} className="rounded-lg" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+type StackProps = {
+  src: string;
+  alt: string;
+};
+
+const StackComponent: React.FC<StackProps> = ({ src, alt }) => {
+  return <img src={src} alt={alt} />;
+};
