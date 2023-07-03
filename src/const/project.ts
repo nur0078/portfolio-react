0f993c8a8aa8c49b51aa8d3af6123fbad2107d36
Anurodh <anurodhp7@gmail.com>
2023-07-03 13:31:28 +1000

    added new projects

--- src/const/project.ts
+++ src/const/project.ts
@@ -1,19 +1,38 @@
-import Project1 from '../assets/project1.png'
-import Project2 from '../assets/project2.png';
+import Pagli from '../assets/pagli.png'
+import Biz from '../assets/bizzz.jpeg';
+import myTravels from '../assets/myTravels.png';
+import Olaf from '../assets/olaf.jpeg';
 
 export const Projects = [
     {
       title: "Pagli Writes",
+      url: "https://pagli-writes.vercel.app/",
       description: "A web application for author.",
-      tools: ["ReactJS", "Tailwind", "TypeScript", "HTML", "Figma", "Git/Github"],
-      img: Project1,
+      tools: ["ReactJS", "Tailwind", "TypeScript", "Figma", "Git/Github"],
+      img: Pagli,
+    },
+    {
+      title: "Travel Journal",
+      url: "https://travel-journal-bice.vercel.app/",
+      description: ` Travel journal thats simply yours`,
+      tools: ["ReactJS", "TypeScript", "Tailwind CSS", "Figma","Git/Github"],
+      img: myTravels,
     },
     {
       title: "Digital Business",
+      url: "https://business-card-virid-omega.vercel.app/",
       description: ` A digital business card.`,
       tools: ["ReactJS", "JavaScript", "CSS", "Figma"],
-      img: Project2,
+      img: Biz,
     },
+    {
+      title: "Don't Melt Olaf",
+      url: "https://dont-melt-olaf.vercel.app/",
+      description: "Word Guessing Game",
+      tools: ["ReactJS", "TypeScript", "Tailwind CSS", "Figma"],
+      img: Olaf,
+    }
+
 
 ];
 
