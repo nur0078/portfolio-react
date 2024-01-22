94b29bf39ab03b04c22ce73b5b3c249713915d28
Anurodh <anurodhp7@gmail.com>
2024-01-22 19:59:07 +1100

    added park-it to projects

--- src/const/project.ts
+++ src/const/project.ts
@@ -1,6 +1,7 @@
 import PagliImg from '../assets/pagli.png'
 import Biz from '../assets/bizzz.jpeg';
 import CarGIF from '../assets/carGIF.gif';
+import ParkIt from "../assets/parkIt.png"
 import myTravelsGIF from '../assets/travel-journal.gif';
 import Olaf from '../assets/olaf.jpeg';
 
@@ -13,19 +14,27 @@ export const Projects = [
       img: CarGIF,
     },
     {
-      title: "Pagli Writes",
-      url: "https://pagli-writes.vercel.app/",
-      description: "A web application for author.",
-      tools: ["ReactJS", "Tailwind", "TypeScript", "Figma", "Git/Github"],
-      img: PagliImg,
-    },
-    {
       title: "Travel Journal",
       url: "https://travel-journal-bice.vercel.app/",
       description: ` Travel journal thats simply yours`,
       tools: ["ReactJS", "TypeScript", "Tailwind CSS", "Figma","Git/Github"],
       img: myTravelsGIF,
     },
+    {
+      title: "Park It",
+      url: "https://park-it-five.vercel.app/",
+      description: ` Parking app that helps you find a spot.`,
+      tools: ["ReactJS", "TypeScript", "Tailwind CSS", "Next","Git/Github"],
+      img: ParkIt,
+    },
+    {
+      title: "Pagli Writes",
+      url: "https://pagli-writes.vercel.app/",
+      description: "A web application for author.",
+      tools: ["ReactJS", "Tailwind", "TypeScript", "Figma", "Git/Github"],
+      img: PagliImg,
+    },
+    
     {
       title: "Digital Business",
       url: "https://business-card-virid-omega.vercel.app/",
