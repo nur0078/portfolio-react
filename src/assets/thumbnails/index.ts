333aec17404850cc3c1b5fa73050701bf62e562d
Anurodh Pandey <anurodhp7@gmail.com>
2024-01-26 02:39:10 +1100

    create index for thumbnail export

---
+++ src/assets/thumbnails/index.ts
@@ -0,0 +1,17 @@
+import Biz from './bizz.jpeg';
+import CarHub from './carHub.jpeg';
+import Nike from './nike.jpeg';
+import Olaf from './olaf.jpeg';
+import PagliImg from "./pagli.jpeg";
+import ParkIt from "./parkIt.jpeg";
+import myTravels from './myTravels.jpeg'
+
+export {
+    Biz,
+    CarHub,
+    Nike,
+    Olaf,
+    PagliImg,
+    ParkIt,
+    myTravels
+};
