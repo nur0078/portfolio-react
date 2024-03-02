41ab1d1789555f955ae4f17ba1c42522b8cc99ea
Anurodh <anurodhp7@gmail.com>
2024-03-03 04:50:12 +1100

    remove deleted thumbnails from import/export

--- src/assets/thumbnails/index.ts
+++ src/assets/thumbnails/index.ts
@@ -1,18 +1,12 @@
-import Biz from './bizz.jpeg';
 import CarHub from './carHub.jpeg';
 import Nike from './nike.jpeg';
-import PagliImg from "./pagli.jpeg";
-import ParkIt from "./parkIt.jpeg";
 import myTravels from './myTravels.jpeg'
 import ResumePage from './resumePage.jpeg'
 
 //export all
 export {
-    Biz,
     CarHub,
     Nike,
-    PagliImg,
-    ParkIt,
     myTravels,
     ResumePage
 };
